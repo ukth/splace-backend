c9a8e70a0689e0b68671d8906e830e622bb19e05
lee1june61 <lee1june61@kaist.ac.kr>
2021-08-25 01:10:50 -0700

    getme

--- folders/getFolders/getFolders.resolvers.js
+++ folders/getFolders/getFolders.resolvers.js
@@ -4,7 +4,7 @@ import { protectedResolver } from "../../users/users.utils";
 
 export default {
   Query: {
-    getFolders: protectedResolver(async (_, { seriesId, lastId }, { loggedInUser }) => {
+    getFolders: protectedResolver(async (_, { lastId }, { loggedInUser }) => {
       try {
         const folders = await client.folder.findMany({
           where: {
