47e053c6060ecb793fdaa043a1e3ee5d313e0a90
lee1june61 <lee1june61@kaist.ac.kr>
2021-09-01 20:15:08 -0700

    fix

--- photologs/getMyLogs/getMyLogs.typeDefs.js
+++ photologs/getMyLogs/getMyLogs.typeDefs.js
@@ -1,12 +1,12 @@
 import { gql } from "apollo-server";
 
 export default gql`
+  type getMyLogsResult {
+    ok: Boolean!
+    error: String
+    mine: [Photolog]
+  }  
   type Query {
-    type getMyLogsResult {
-      ok: Boolean!
-      error: String
-      mine: [Photolog]
-    }
     getMyLogs(lastId: Int): getMyLogsResult!
   }
 `;
