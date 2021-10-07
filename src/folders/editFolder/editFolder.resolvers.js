844f0eb11dbfdbcfc62f6f2b9f55fc27a1e839d8
lee1june61 <lee1june61@kaist.ac.kr>
2021-10-07 01:24:38 -0700

    ...

---
+++ src/folders/editFolder/editFolder.resolvers.js
@@ -0,0 +1,55 @@
+import client from "../../client";
+import { protectedResolver } from "../../users/users.utils";
+
+export default {
+  Mutation: {
+    editFolder: protectedResolver(async (
+      _,
+      { folderId, title },
+      { loggedInUser }
+    ) => {
+      try {
+
+        const ok = await client.folder.findFirst({
+          where: {
+            id: folderId,
+            members: {
+              some: {
+                id: loggedInUser.id
+              }
+            }
+          },
+        })
+
+        //console.log(ok);
+        if (!ok) {
+          return {
+            ok: false,
+            error: "you dont have authentication to add save."
+          };
+        }
+
+        
+        const a = await client.folder.update({
+          where: {
+            id: folderId
+          },
+          data: {
+            title,
+          },
+        });
+        
+        //console.log(a);
+        return {
+          ok: true,
+        };
+      } catch (e) {
+        console.log(e);
+        return {
+          ok: false,
+          error: "cant create folder",
+        };
+      }
+    }),
+  }
+};
