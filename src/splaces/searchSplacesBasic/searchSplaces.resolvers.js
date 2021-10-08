bc3d4d064bc5d53f5da4fc64cfd84f6a91ee2283
lee1june61 <lee1june61@kaist.ac.kr>
2021-10-07 23:25:55 -0700

    ...

--- src/splaces/searchSplacesBasic/searchSplaces.resolvers.js
+++ src/splaces/searchSplacesBasic/searchSplaces.resolvers.js
@@ -50,7 +50,7 @@ export default {
               "filter": [
                 {
                   "terms": {
-                    "stringC": ["1 2"],
+                    "stringC": ["1 2 "],
                   }
                 },
               ],
