fa23de807d5436113dff558d078c04fabf2b22f8
lee1june61 <lee1june61@kaist.ac.kr>
2021-10-21 01:54:11 -0700

    ...

--- src/users/createAccount/createAccount.resolvers.js
+++ src/users/createAccount/createAccount.resolvers.js
@@ -24,7 +24,7 @@ export default {
   Mutation: {
     createAccount: async (
       _,
-      { username, password, phone }
+      { username, password, phone, certificate }
     ) => {
       try {
         const existingUser = await client.user.findFirst({
