9b9c577a6d7a94f2e3b1fb5688bbac88f35e7ed8
lee1june61 <lee1june61@kaist.ac.kr>
2021-10-29 13:44:20 +0900

    ...

--- src/redis.js
+++ src/redis.js
@@ -1,8 +1,8 @@
 var Redis = require("ioredis");
 require("dotenv").config();
 
-const host =  process.env.REDIS_URL
-const port =  process.env.REDIS_PORT
+const host = process.env.REDIS_URL
+const port = process.env.REDIS_PORT
 const password = process.env.REDIS_PASSWORD
 
 var nodes = [{
@@ -10,10 +10,14 @@ var nodes = [{
   port
 }]
 
-var redisClient = new Redis.Cluster(nodes,{
+var redisClient = new Redis.Cluster(nodes, {
+  dnsLookup: (address, callback) => {
+    console.log('====[dnsLookup]====');
+    return callback(null, address);
+  },
   redisOptions: {
     password,
-    tls:{}
+    tls: {}
   }
 })
 
