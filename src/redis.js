41d58959c210f9518d6dabd8911400b0cd6c574c
lee1june61 <lee1june61@kaist.ac.kr>
2021-10-29 14:07:21 +0900

    ..

--- src/redis.js
+++ src/redis.js
@@ -5,20 +5,12 @@ const host = process.env.REDIS_URL
 const port = process.env.REDIS_PORT
 const password = process.env.REDIS_PASSWORD
 
-var nodes = [{
+var redisClient = new Redis({
   host,
-  port
-}]
+  port,
+  password,
+  tls: {}
 
-var redisClient = new Redis.Cluster(nodes, {
-  dnsLookup: (address, callback) => {
-    console.log('====[dnsLookup]====');
-    return callback(null, address);
-  },
-  redisOptions: {
-    password,
-    tls: {}
-  }
 })
 
 export default redisClient
