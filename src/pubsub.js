9c5f11d75e9aa1f12eafcd33f564541cc482c165
lee1june61 <lee1june61@kaist.ac.kr>
2021-10-29 20:21:54 +0900

    ...

--- src/pubsub.js
+++ src/pubsub.js
@@ -1,18 +1,13 @@
-/*require("dotenv").config();
+require("dotenv").config();
 import { RedisPubSub } from 'graphql-redis-subscriptions';
 
 const pubsub = new RedisPubSub({
   connection: {
     host: process.env.REDIS_URL,
     port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
     retry_strategy: options => Math.max(options.attempt * 100, 3000),
   },
 });
-
-export default pubsub;*/
-
-import { PubSub } from 'graphql-subscriptions';
-
-const pubsub = new PubSub();
 
 export default pubsub;
